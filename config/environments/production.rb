6efa46e73461d08d2e5536ef63ba6758a91c270c
fanda <fandisek@gmail.com>
2015-04-27 21:32:53 +0200

    Region aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Eshop::Application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => 'eu-west-1'
+      :endpoint => 's3-eu-west-1'
     }
   }
 
